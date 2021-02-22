ffe32eed87e33fbae769361b11ca9bd01c67c235
Michael Connor <michaelconnor468@gmail.com>
2021-02-22 18:21:36 -0500

    added generic constructor that can accept arbitrary dimensions

--- board.py
+++ board.py
@@ -4,18 +4,22 @@ Will perform integrity checks on additions and reject them if they are invalid u
 """
 class Board:
     values = []
-    length = 0
-    width = 0
-    subsquareLength = 0
-    subsquareWidth = 0
+    length = 9
+    width = 9
+    subsquareLength = 3
+    subsquareWidth = 3
+    minValue = 1
+    maxValue = 9
 
-    def __init__(self):
-        self.length = 9
-        self.width = 9
-        self.subsquareLength = 3
-        self.subsquareWidth = 3
+    def __init__(self, length = 9, width = 9, subsquareLength = 3, subsquareWidth = 3):
+        self.length = length
+        self.width = width
+        self.subsquareLength = subsquareLength
+        self.subsquareWidth = subsquareWidth
+        assert length % subsquareLength == 0, 'length must be divisible by subsquareLength'
+        assert width % subsquareWidth == 0, 'width must be divisible by subsquareWidth'
         self.initEmptyValues()
-
+        self.maxValue = max(subsquareLength*subsquareWidth, length, width)
 
     def initEmptyValues(self):
         for i in range(0, self.length):
@@ -30,3 +34,5 @@ if __name__ == '__main__':
     for x in board.values:
         for y in x:
             assert y is None, 'All values in an initial board should be None'
+    board = Board(10, 8, 5, 2)
+    assert board.maxValue == 10, 'Board should initialize the correct max value'
